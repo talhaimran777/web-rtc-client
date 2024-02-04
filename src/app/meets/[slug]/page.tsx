243018e5e114e8b06b5a82aa6b4278426fe0d6b3
Talha Imran <talhaimran284@gmail.com>
2024-02-04 23:18:02 +0500

    ui: fixed remote videos rendering issue

--- src/app/meets/[slug]/page.tsx
+++ src/app/meets/[slug]/page.tsx
@@ -11,202 +11,6 @@ const socket = io(serverURL)
 const peers = new Map<string, RTCPeerConnection>()
 let stream: MediaStream
 
-/**
- * Create a new peer connection
- * @param socketId - The socket id of the user to create a peer connection with
- * @returns - Promise<RTCPeerConnection>
- *
- */
-const createNewPeerConnection = async (
-    socketId: string
-): Promise<RTCPeerConnection> => {
-    const response = await fetch(
-        `${process.env.NEXT_PUBLIC_METERED_CREDS_URL}?apiKey=${process.env.NEXT_PUBLIC_METERED_API_KEY}`
-    )
-
-    const iceServers = await response.json()
-
-    console.log(
-        `Creating a new peer connection for user ${socket.id} to be able to connect to user ${socketId}`
-    )
-
-    let pc = new RTCPeerConnection({
-        iceServers: iceServers,
-    })
-
-    if (stream) {
-        stream.getTracks().forEach((track) => {
-            console.log('Adding track to peer', pc)
-            pc.addTrack(track, stream)
-        })
-    }
-
-    pc.ontrack = (e) => {
-        console.log('Track Received', e, socketId)
-        console.log('Streams', e.streams)
-
-        e.streams.forEach((stream) => {
-            console.log('Stream', stream)
-
-            // Todo: Add a check for video type
-            // Create a video element
-            const videoElement = document.createElement('video')
-
-            // Set attributes for the video element
-            videoElement.id = socketId
-            videoElement.autoplay = true
-            videoElement.playsInline = true
-            videoElement.controls = false
-            videoElement.className = 'w-full bg-slate-900 rounded-lg'
-
-            videoElement.srcObject = stream
-
-            // Append the video element to a container (assuming a div with id 'videoContainer' exists)
-            const element = document.getElementById('video-container')
-
-            if (element) {
-                element.appendChild(videoElement)
-            }
-        })
-    }
-
-    pc.onicecandidate = (event) => {
-        console.log('Ice Candidate Received')
-
-        if (event.candidate) {
-            console.log(`Sending Ice Candidate to ${socketId}`, event.candidate)
-            socket.emit('ice-candidate', event.candidate, socketId)
-        }
-    }
-
-    return pc
-}
-
-/**
- * Handler: when an offer is received
- * @param offer - The offer
- * @param from - The socket id of the user who sent the offer
- */
-const handleOffer = async (offer: RTCSessionDescription, from: string) => {
-    console.log(`Offer Received from ${from}`, offer)
-
-    const pc = await createNewPeerConnection(from)
-
-    console.log('Setting Remote Description with this offer', offer)
-    await pc.setRemoteDescription(new RTCSessionDescription(offer))
-
-    const answer = await pc.createAnswer()
-
-    console.log('Setting Local Description with this answer', answer)
-    await pc.setLocalDescription(new RTCSessionDescription(answer))
-
-    socket.emit('answer', answer, from)
-
-    peers.set(from, pc)
-}
-
-/**
- * Handler: when an answer is received
- * @param answer - The answer
- * @param from - The socket id of the user who sent the answer
- */
-const handleAnswer = async (answer: RTCSessionDescription, from: string) => {
-    console.log(`Answer received from ${from}`, answer)
-
-    const peer = peers.get(from)
-
-    if (!peer) {
-        console.error('no peerconnection')
-        return
-    }
-
-    if (peer.signalingState !== 'stable') {
-        console.log('Setting Remote Description with this answer', answer)
-        await peer.setRemoteDescription(new RTCSessionDescription(answer))
-    }
-}
-
-/**
- * Handler: when an ice candidate is received
- * @param candidate - The ice candidate
- * @param from - The socket id of the user who sent the ice candidate
- */
-const handleIceCandidate = async (candidate: RTCIceCandidate, from: string) => {
-    console.log(`Ice Candidate Received from ${from}`, candidate)
-
-    const peer = peers.get(from)
-
-    if (!peer) {
-        console.error('no peerconnection')
-        return
-    }
-
-    if (!candidate.candidate) {
-        await peer.addIceCandidate(undefined)
-    } else {
-        console.log('Adding Ice Candidate', candidate)
-        await peer.addIceCandidate(candidate)
-    }
-}
-
-/**
- * Handler: when a new user joins
- * @param socketId - The socket id of the user who joined
- */
-const handleUserJoined = async (socketId: string) => {
-    console.log('A new user joined with socket id ', socketId)
-
-    const pc = await createNewPeerConnection(socketId)
-
-    console.log(`Creating an offer from ${socket.id} to ${socketId}`)
-    const offer = await pc.createOffer()
-
-    console.log('Setting Local Description with this offer', offer)
-    await pc.setLocalDescription(new RTCSessionDescription(offer))
-
-    socket.emit('offer', offer, socketId)
-
-    peers.set(socketId, pc)
-}
-
-/**
- * Handler: when a user leaves
- * @param socketId - The socket id of the user who left
- */
-const handleUserLeft = (socketId: string) => {
-    console.log('User Left', socketId)
-    console.log('Before Remove', peers)
-    const peer = peers.get(socketId)
-
-    if (peer) {
-        peer.close()
-        peers.delete(socketId)
-        const videoToRemove = document.getElementById(socketId)
-
-        if (videoToRemove) {
-            document
-                .getElementById('video-container')
-                ?.removeChild(videoToRemove)
-        }
-    }
-
-    console.log('After Remove', peers)
-}
-
-socket.on('connect', () => {
-    console.log('Socket Connected')
-})
-
-socket.on('user-joined', handleUserJoined)
-
-socket.on('user-left', handleUserLeft)
-
-socket.on('offer', handleOffer)
-
-socket.on('answer', handleAnswer)
-
-socket.on('ice-candidate', handleIceCandidate)
-
 const Lobby = () => {
     const { slug } = useParams()
 
@@ -215,6 +19,193 @@ const Lobby = () => {
 
     const videoRef = useRef<HTMLVideoElement>(null)
     const videoContainerRef = useRef<HTMLDivElement>(null)
+
+    /**
+     * Create a new peer connection
+     * @param socketId - The socket id of the user to create a peer connection with
+     * @returns - Promise<RTCPeerConnection>
+     *
+     */
+    const createNewPeerConnection = async (
+        socketId: string
+    ): Promise<RTCPeerConnection> => {
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_METERED_CREDS_URL}?apiKey=${process.env.NEXT_PUBLIC_METERED_API_KEY}`
+        )
+
+        const iceServers = await response.json()
+
+        console.log(
+            `Creating a new peer connection for user ${socket.id} to be able to connect to user ${socketId}`
+        )
+
+        let pc = new RTCPeerConnection({
+            iceServers: iceServers,
+        })
+
+        if (stream) {
+            stream.getTracks().forEach((track) => {
+                console.log('Adding track to peer', pc)
+                pc.addTrack(track, stream)
+            })
+        }
+
+        pc.ontrack = (e) => {
+            console.log('Track Received', e, socketId)
+            console.log('Streams', e.streams)
+
+            e.streams.forEach((stream) => {
+                console.log('Stream', stream)
+
+                // Todo: Add a check for video type
+                // Create a video element
+                const videoElement = document.createElement('video')
+
+                // Set attributes for the video element
+                videoElement.id = socketId
+                videoElement.autoplay = true
+                videoElement.playsInline = true
+                videoElement.controls = false
+                videoElement.className = 'rounded-lg'
+                videoElement.srcObject = stream
+
+                if (videoContainerRef && videoContainerRef.current) {
+                    videoContainerRef.current.appendChild(videoElement)
+                }
+            })
+        }
+
+        pc.onicecandidate = (event) => {
+            console.log('Ice Candidate Received')
+
+            if (event.candidate) {
+                console.log(
+                    `Sending Ice Candidate to ${socketId}`,
+                    event.candidate
+                )
+                socket.emit('ice-candidate', event.candidate, socketId)
+            }
+        }
+
+        return pc
+    }
+
+    /**
+     * Handler: when an offer is received
+     * @param offer - The offer
+     * @param from - The socket id of the user who sent the offer
+     */
+    const handleOffer = async (offer: RTCSessionDescription, from: string) => {
+        console.log(`Offer Received from ${from}`, offer)
+
+        const pc = await createNewPeerConnection(from)
+
+        console.log('Setting Remote Description with this offer', offer)
+        await pc.setRemoteDescription(new RTCSessionDescription(offer))
+
+        const answer = await pc.createAnswer()
+
+        console.log('Setting Local Description with this answer', answer)
+        await pc.setLocalDescription(new RTCSessionDescription(answer))
+
+        socket.emit('answer', answer, from)
+
+        peers.set(from, pc)
+    }
+
+    /**
+     * Handler: when an answer is received
+     * @param answer - The answer
+     * @param from - The socket id of the user who sent the answer
+     */
+    const handleAnswer = async (
+        answer: RTCSessionDescription,
+        from: string
+    ) => {
+        console.log(`Answer received from ${from}`, answer)
+
+        const peer = peers.get(from)
+
+        if (!peer) {
+            console.error('no peerconnection')
+            return
+        }
+
+        if (peer.signalingState !== 'stable') {
+            console.log('Setting Remote Description with this answer', answer)
+            await peer.setRemoteDescription(new RTCSessionDescription(answer))
+        }
+    }
+
+    /**
+     * Handler: when an ice candidate is received
+     * @param candidate - The ice candidate
+     * @param from - The socket id of the user who sent the ice candidate
+     */
+    const handleIceCandidate = async (
+        candidate: RTCIceCandidate,
+        from: string
+    ) => {
+        console.log(`Ice Candidate Received from ${from}`, candidate)
+
+        const peer = peers.get(from)
+
+        if (!peer) {
+            console.error('no peerconnection')
+            return
+        }
+
+        if (!candidate.candidate) {
+            await peer.addIceCandidate(undefined)
+        } else {
+            console.log('Adding Ice Candidate', candidate)
+            await peer.addIceCandidate(candidate)
+        }
+    }
+
+    /**
+     * Handler: when a new user joins
+     * @param socketId - The socket id of the user who joined
+     */
+    const handleUserJoined = async (socketId: string) => {
+        console.log('A new user joined with socket id ', socketId)
+
+        const pc = await createNewPeerConnection(socketId)
+
+        console.log(`Creating an offer from ${socket.id} to ${socketId}`)
+        const offer = await pc.createOffer()
+
+        console.log('Setting Local Description with this offer', offer)
+        await pc.setLocalDescription(new RTCSessionDescription(offer))
+
+        socket.emit('offer', offer, socketId)
+
+        peers.set(socketId, pc)
+    }
+
+    /**
+     * Handler: when a user leaves
+     * @param socketId - The socket id of the user who left
+     */
+    const handleUserLeft = (socketId: string) => {
+        console.log('User Left', socketId)
+        console.log('Before Remove', peers)
+        const peer = peers.get(socketId)
+
+        if (peer) {
+            peer.close()
+            peers.delete(socketId)
+            const videoToRemove = document.getElementById(socketId)
+
+            if (videoToRemove) {
+                document
+                    .getElementById('video-container')
+                    ?.removeChild(videoToRemove)
+            }
+        }
+
+        console.log('After Remove', peers)
+    }
 
     const getPermissions = async () => {
         try {
@@ -233,6 +224,36 @@ const Lobby = () => {
         setJoined(true)
     }
 
+    const videoElement = (
+        <video
+            ref={videoRef}
+            className='bg-black rounded-md'
+            autoPlay
+            playsInline
+            controls={false}
+        ></video>
+    )
+
+    useEffect(() => {
+        return () => {
+            socket.close()
+        }
+    }, [])
+
+    useEffect(() => {
+        if (!socket) return
+
+        socket.on('user-joined', handleUserJoined)
+
+        socket.on('user-left', handleUserLeft)
+
+        socket.on('offer', handleOffer)
+
+        socket.on('answer', handleAnswer)
+
+        socket.on('ice-candidate', handleIceCandidate)
+    }, [socket])
+
     useEffect(() => {
         getPermissions()
     }, [])
@@ -248,17 +269,6 @@ const Lobby = () => {
             console.log('Video Container', videoContainerRef.current)
         }
     }, [videoContainerRef.current])
-
-
-    const videoElement = (
-        <video
-            ref={videoRef}
-            className='w-full h-full bg-black rounded-md'
-            autoPlay
-            playsInline
-            controls={false}
-        ></video>
-    )
 
     return (
         <div className='flex justify-center items-center min-h-screen p-4 bg-slate-800 text-white'>
@@ -282,7 +292,7 @@ const Lobby = () => {
                     )}
                 </div>
             ) : (
-                <div className='w-full lg:max-w-xl'>
+                <div className='w-full lg:max-w-2xl'>
                     <div
                         ref={videoContainerRef}
                         id='video-container'
